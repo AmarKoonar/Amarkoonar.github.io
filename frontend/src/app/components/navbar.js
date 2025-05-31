9b23b0f6e772c70404f7c2fe219c96f68832944b
AmarKoonar <amarkoonar@gmail.com>
2025-05-31 13:32:17 -0700

    added basic navbar and added navbar component to page.js

---
+++ frontend/src/app/components/navbar.js
@@ -0,0 +1,22 @@
+"use client";
+
+export default function navbar() {
+  return (
+    <div className="group fixed w-full -top-16 transition-transform duration-300 hover:translate-y-15 z-50">
+      <div>
+            <nav className="p-3 sm:p-4 text-white flex flex-wrap justify-between items-center">
+                <h1 className="text-xl sm:text-2xl font-bold">Amar Koonar</h1>
+                <div className="space-x-2 sm:space-x-4 text-sm sm:text-base">
+                <a href="#home" className="hover:underline shadow-xl">Home</a>
+                <a href="#about" className="hover:underline shadow-xl ">About</a>
+                <a href="#about" className="hover:underline shadow-xl ">Projects</a>
+                <a href="#contact" className="hover:underline shadow-xl ">Resume</a>
+                <a href="#contact" className="hover:underline shadow-xl ">contact</a>
+                </div>
+        </nav>
+        <div className="h-16 w-full p-3 sm:p-4 text-white flex justify-between items-center">
+        </div>
+      </div>
+    </div>
+  );
+}
