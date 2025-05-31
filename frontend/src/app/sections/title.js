6e609eb0c30ed65ef791f44a774d245a7302f675
AmarKoonar <amarkoonar@gmail.com>
2025-05-31 12:42:00 -0700

    Created title page with small discription

---
+++ frontend/src/app/sections/title.js
@@ -0,0 +1,121 @@
+"use client";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+export default function Titlepage() {
+  const [visible, setVisible] = useState(true);
+
+  const TitleVariants = [
+    {
+      start: { x: -1000, opacity: 0 },
+      end: { x: 0, opacity: 1, scale: 1, transition: { type: "spring", stiffness: 100 } },
+      exit: { x: -1000, opacity: 0, transition: { duration: 0.5 } },
+    },
+    {
+      start: { x: 1000, opacity: 0 },
+      end: { x: 0, opacity: 1, scale: 1, transition: { type: "spring", stiffness: 100 } },
+      exit: { x: 1000, opacity: 0, transition: { duration: 0.5 } },
+    },
+    {
+      start: { y: -1000, opacity: 0 },
+      end: { y: 0, opacity: 1, scale: 1, transition: { type: "spring", stiffness: 100 } },
+      exit: { y: -1000, opacity: 0, transition: { duration: 0.5 } },
+    },
+    {
+      start: { y: 1000, opacity: 0 },
+      end: { y: 0, opacity: 1, scale: 1, transition: { type: "spring", stiffness: 100 } },
+      exit: { y: 1000, opacity: 0, transition: { duration: 0.5 } },
+    },
+    {
+      start: { scale: 5, opacity: 0 },
+      end: { scale: 1, opacity: 1, transition: { type: "spring", stiffness: 100 } },
+      exit: { scale: 5, opacity: 0, transition: { duration: 0.5 } },
+    },
+  ];
+
+  const diseVariants = [
+    {
+      exit: { x: -1000, opacity: 0, transition: { duration: 0.5 } },
+    },
+    {
+      exit: { x: 1000, opacity: 0, transition: { duration: 0.5 } },
+    },
+    {
+      exit: { y: -1000, opacity: 0, transition: { duration: 0.5 } },
+    },
+    {
+      exit: { y: 1000, opacity: 0, transition: { duration: 0.5 } },
+    },
+    {
+      exit: { scale: 5, opacity: 0, transition: { duration: 0.5 } },
+    },
+  ];
+
+  const Title = ["H", "i", ",", " ", "I", "'", "m", " ", "A", "m", "a", "r", " ", "K", "o", "o", "n", "a", "r"];
+
+  const title_dis = [
+    "I", "'", "m", " ", "a", " ", "p", "r", "o", "g", "r", "a", "m", "m", "e", "r", " ",
+    "w", "h", "o", " ", "l", "i", "k", "e", "s", " ", "t", "o", " ",
+    "l", "e", "a", "r", "n", " ", "n", "e", "w", " ", "t", "h", "i", "n", "g", "s", "."
+  ];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {  
+        setVisible(false);
+      }
+      else{
+        setVisible(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <>
+      <div className="flex bg-black flex-col justify-center items-center h-screen">
+        <AnimatePresence>
+          {visible && (
+            <>
+
+              <div className="text-white flex flex-wrap justify-center text-2xl sm:text-6xl md:text-8xl mb-4 text-center w-full px-4">
+
+                {Title.map((letter, index) => (
+                  <motion.span
+                    key={`title-${index}`}
+                    variants={TitleVariants[index % TitleVariants.length]}
+                    initial="start"
+                    animate="end"
+                    exit="exit"
+                    className="mx-2"
+                  >
+                    {letter}
+                  </motion.span>
+                ))}
+              </div>
+
+
+              <div className="text-white flex px-2 text-1xl lg:text-3xl font-bold flex-wrap justify-center mb-8">
+                {title_dis.map((letter, index) => (
+                  <motion.span
+                    key={`title-dis-${index}`}
+                    variants={diseVariants[index % diseVariants.length]}
+                    initial={{ y: 1000, opacity: 1 }}
+                    animate={{ y: 0, opacity: 1, scale: 1, transition: { duration: 0.4, delay: 1 + index * 0.05 } }}
+                    exit="exit"
+                    className="mx-1"
+                  >
+                    {letter}
+                  </motion.span>
+                ))}
+              </div>
+            </>
+          )}
+        </AnimatePresence>
+        
+      </div>
+    </>
+  );
+}
