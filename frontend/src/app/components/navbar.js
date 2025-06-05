343088dec39062246229a9986fea82a0e3bc8912
AmarKoonar <amarkoonar@gmail.com>
2025-06-05 00:20:16 -0700

    navbar: changed the ui for navbar and made it look more modern

--- frontend/src/app/components/navbar.js
+++ frontend/src/app/components/navbar.js
@@ -8,10 +8,10 @@ export default function navbar() {
     }
   };
   return (
-    <div className="group fixed w-full -top-18 transition duration-300 hover:translate-y-15 z-50">
-            <nav className="p-3 sm:p-4 text-white flex flex-wrap justify-between items-center rounded-xl duration-500 shadow-xl/30 bg-slate-950 group-hover:shadow-[#00FFDE] border-[#00FFDE] lg:border-3 border-1">
-                <h1 className="text-xl sm:text-2xl font-bold">Amar Koonar</h1>
-                <div className="space-x-2 sm:space-x-4 text-sm sm:text-base">
+    <div className="group fixed w-full transition duration-300 z-50">
+      <div className="flex justify-center">
+            <nav className=" p-3 text-white flex flex-wrap justify-between items-center rounded-full duration-500 shadow-xl/30 bg-slate-950 group-hover:shadow-[#00FFDE] hover:border-[#00FFDE] border-[#00CAFF] lg:border-3 border-1">
+                <div className="space-x-2 p-0 sm:space-x-4 text-sm sm:text-base">
                 <a onClick={() => scrollToId("Title")} className="hover:cursor-pointer hover:underline shadow-xl">Home</a>
                 <a onClick={() => scrollToId("Aboutme")}  className="hover:cursor-pointer hover:underline shadow-xl ">About</a>
                 <a onClick={() => scrollToId("Projects")} className="hover:cursor-pointer hover:underline shadow-xl ">Projects</a>
@@ -20,9 +20,6 @@ export default function navbar() {
                 <a onClick={() => scrollToId("Contact")} className="hover:cursor-pointer hover:underline shadow-xl ">Contact</a>
                 </div>
         </nav>
-        <div className="h-16 w-full p-3 sm:p-4 text-white flex justify-between items-center">
-            <img src="/navbar1.png" alt="navbar icon" className="w-5 h-5">
-            </img>
         </div>
       
     </div>
