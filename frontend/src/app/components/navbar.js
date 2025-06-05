998372166fd50ce440c85c2a02840ed40b2b9f14
AmarKoonar <amarkoonar@gmail.com>
2025-06-04 21:25:24 -0700

    navbar: Changed colors to match new theme

--- frontend/src/app/components/navbar.js
+++ frontend/src/app/components/navbar.js
@@ -3,7 +3,7 @@
 export default function navbar() {
   return (
     <div className="group fixed w-full -top-18 transition duration-300 hover:translate-y-15 z-50">
-            <nav className="p-3 sm:p-4 text-white flex flex-wrap justify-between items-center rounded-xl border-indigo-500 lg:border-3 border-1">
+            <nav className="p-3 sm:p-4 text-white flex flex-wrap justify-between items-center rounded-xl duration-500 shadow-xl/30 bg-slate-950 group-hover:shadow-[#00FFDE]  border-[#00FFDE] lg:border-3 border-1">
                 <h1 className="text-xl sm:text-2xl font-bold">Amar Koonar</h1>
                 <div className="space-x-2 sm:space-x-4 text-sm sm:text-base">
                 <a href="#home" className="hover:underline shadow-xl">Home</a>
