9cdda76d620b3d12448d6b51d472cfce5cb200a4
AmarKoonar <amarkoonar@gmail.com>
2025-06-10 18:30:20 -0700

    main: reverted code to older commit due to problems with new code

--- frontend/src/app/components/footer.js
+++ frontend/src/app/components/footer.js
@@ -1,22 +1,29 @@
 "use client";
 
 export default function Footer() {
+  const scrollToId = (id) => {
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className="w-full bg-black transition duration-300 z-50">
 
             <nav className=" p-3 text-white flex flex-col items-center justify-center duration-500 shadow-xl/30 bg-slate-950 group-hover:shadow-[#00FFDE] border-0 hover:border-[#00FFDE] border-t-[#00CAFF] lg:border-t-2 border-t-1">
                 <div className="space-x-2 p-0 sm:space-x-4 text-sm sm:text-base mb-7">
-                <a href="#Title" className="hover:cursor-pointer hover:underline shadow-xl">Home</a>
+                <a onClick={() => scrollToId("Title")} className="hover:cursor-pointer hover:underline shadow-xl">Home</a>
                 <a>|</a>
-                <a href="#Aboutme" className="hover:cursor-pointer hover:underline shadow-xl ">About</a>
+                <a onClick={() => scrollToId("Aboutme")}  className="hover:cursor-pointer hover:underline shadow-xl ">About</a>
                 <a>|</a>
-                <a href="#Projects"  className="hover:cursor-pointer hover:underline shadow-xl ">Projects</a>
+                <a onClick={() => scrollToId("Projects")} className="hover:cursor-pointer hover:underline shadow-xl ">Projects</a>
                 <a>|</a>
-                <a href="#Coursework"  className="hover:cursor-pointer hover:underline shadow-xl ">Coursework</a>
+                <a onClick={() => scrollToId("Coursework")} className="hover:cursor-pointer hover:underline shadow-xl ">Coursework</a>
                 <a>|</a>
-                <a href="#Resume"  className="hover:cursor-pointer hover:underline shadow-xl ">Resume</a>
+                <a onClick={() => scrollToId("Resume")}  className="hover:cursor-pointer hover:underline shadow-xl ">Resume</a>
+                
                 <a>|</a>
-                <a href="#Contact"   className="hover:cursor-pointer hover:underline shadow-xl ">Contact</a>
+                <a onClick={() => scrollToId("Contact")} className="hover:cursor-pointer hover:underline shadow-xl ">Contact</a>
 
                 </div>
                 <div className="space-x-2 p-0 sm:space-x-4 text-sm sm:text-base">
--- frontend/src/app/components/navbar.js
+++ frontend/src/app/components/navbar.js
@@ -1,17 +1,23 @@
 "use client";
 
 export default function navbar() {
+  const scrollToId = (id) => {
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className="group fixed w-full transition duration-300 z-50">
       <div className="flex justify-center">
             <nav className=" p-3 text-white flex flex-wrap justify-between items-center rounded-full duration-500 shadow-xl/30 bg-slate-950 group-hover:shadow-[#00FFDE] hover:border-[#00FFDE] border-[#00CAFF] lg:border-3 border-1">
                 <div className="space-x-2 p-0 sm:space-x-4 text-sm sm:text-base">
-                <a href="#Title" className="hover:cursor-pointer hover:underline shadow-xl">Home</a>
-                <a href="#Aboutme" className="hover:cursor-pointer hover:underline shadow-xl ">About</a>
-                <a href="#Projects"  className="hover:cursor-pointer hover:underline shadow-xl ">Projects</a>
-                <a href="#Coursework"  className="hover:cursor-pointer hover:underline shadow-xl ">Coursework</a>
-                <a href="#Resume"  className="hover:cursor-pointer hover:underline shadow-xl ">Resume</a>
-                <a href="#Contact"   className="hover:cursor-pointer hover:underline shadow-xl ">Contact</a>
+                <a onClick={() => scrollToId("Title")} className="hover:cursor-pointer hover:underline shadow-xl">Home</a>
+                <a onClick={() => scrollToId("Aboutme")}  className="hover:cursor-pointer hover:underline shadow-xl ">About</a>
+                <a onClick={() => scrollToId("Projects")} className="hover:cursor-pointer hover:underline shadow-xl ">Projects</a>
+                <a onClick={() => scrollToId("Coursework")} className="hover:cursor-pointer hover:underline shadow-xl ">Coursework</a>
+                <a onClick={() => scrollToId("Resume")}  className="hover:cursor-pointer hover:underline shadow-xl ">Resume</a>
+                <a onClick={() => scrollToId("Contact")} className="hover:cursor-pointer hover:underline shadow-xl ">Contact</a>
                 </div>
         </nav>
         </div>
